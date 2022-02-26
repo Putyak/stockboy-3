7db36719a1063fb6e696219cdea1f54d4c1d1caf
Jason Davis <jason.h.davis@gmail.com>
2022-02-26 15:12:37 -0500

    WIP sales by sku data from api

--- client/src/components/Cards/CardSimpleDatatable.svelte
+++ client/src/components/Cards/CardSimpleDatatable.svelte
@@ -1,26 +1,38 @@
 <script>
-    import { data } from '../../Dummy/TableData.js'  
+    //import { data } from '../../Dummy/TableData.js'
     import { Datatable, SearchInput } from 'svelte-simple-datatables'
-
     import { CSVDownloader } from 'svelte-csv';
 
-    
-    const settings = { 
-        pagination: true, 
+
+
+    const settings = {
+        pagination: true,
         scrollY: false,
         rowsPerPage: 25,
         sortable: true,
         css: true,
         blocks: {
-            searchInput: false, 
+            searchInput: false,
             paginationButtons: true,
             paginationRowCount: true,
         }
     }
 
     let rows
+
+    //let sales_by_sku = fetch('http://127.0.0.1:5555/sales/by-sku')
+     //   .then(Response => Response.json());
+     //   return Response.json();
+        //.then(Response => (data = Response));
+
     
-    
+    //let data = getData();
+	let sales_by_sku = (async () => {
+		let response = await fetch('http://127.0.0.1:5555/sales/by-sku')
+    return await response.json()
+	})()
+
+    //console.log(sales_by_sku);
 </script>
 
 
@@ -38,44 +50,57 @@
 
           <div class="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
             <CSVDownloader class="hover:bg-purple-600"
-                data= {data} filename={'stockboy-report.csv'} bom={true}
+                data= {sales_by_sku} filename={'stockboy-report.csv'} bom={true}
                 >CSV <i class="fas fa-download py-2"></i>
             </CSVDownloader>
 
           </div>
         </div>
+
+    {#await sales_by_sku }
+        <p> loading</p>
     
-    <Datatable {settings} {data} bind:dataRows={rows} classList="w-full">
+    {:then} 
+        
+   
+    <Datatable {settings} {sales_by_sku} bind:dataRows={sales_by_sku} classList="w-full">
         <thead class="mt-6 w-full">
-            <th data-key="sku" 
+            <th data-key="sku"
                 class="px-6 sortable bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                 sku
                 <i class="fas fa-sort"></i>
             </th>
-            <th data-key="name" 
+            <th data-key="name"
                 class="px-6 sortable bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                 Name
                 <i class="fas fa-sort"></i>
             </th>
-            <th data-key="qty" 
+            <th data-key="qty"
                 class="px-6 sortable bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                 Qty Sold
                 <i class="fas fa-sort"></i>
             </th>
-            <th data-key="sales" 
+            <th data-key="sales"
                 class="px-6 sortable bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                 Sales
                 <i class="fas fa-sort"></i>
             </th>
-            <th data-key="burn" 
+            <th data-key="sales"
+                 class="px-6 sortable bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                Inventory
+                <i class="fas fa-sort"></i>
+            </th>
+            <th data-key="burn"
                 class="px-6 sortable bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
                 Burn
                 <i class="fas fa-sort"></i>
             </th>
         </thead>
         <tbody>
+
         {#if rows}
             {#each $rows as row}
+            
             <tr>
                 <td class="border-t-0 px-6 align-middle text-center border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
                     <strong class="text-center">{row.sku}</strong> {#if row.img != "None"}<img src="{row.img}" width=100px class="pt-2 rounded mx-auto text-center"> {:else}<br/><i class="fas fa-image fa-4x"></i> {/if}</td>
@@ -86,11 +111,14 @@
                 <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-s whitespace-nowrap p-4">
                     ${row.sales}</td>
                 <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-s whitespace-nowrap p-4">
+ </td>
+                <td class="border-t-0 px-6 align-middle border-l-0 border-r-0 text-s whitespace-nowrap p-4">
                     {row.burn}</td>
             </tr>
             {/each}
         {/if}
         </tbody>
     </Datatable>
+    {/await}
 </div>
 </div>
